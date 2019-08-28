dffe8a66ec85bbb099018dde789ab7bf2f0a2197
X-olani <xolani.sk.yona@gmail.com>
2019-08-28 22:38:17 +0200

    blog

---
+++ precache-manifest.189844f2f6a3298ebc9ae8b4daa122dd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd4f97e84c23ef655b88e62c48053ac5",
+    "url": "/static/media/coffee-communication.cd4f97e8.jpg"
+  },
+  {
+    "revision": "03f09eb6d6a0f04af2a9",
+    "url": "/static/css/main.66f8fa75.chunk.css"
+  },
+  {
+    "revision": "e3f3861250fbc0aa1bac",
+    "url": "/static/js/1.e3f38612.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c53ad51cde2d7051f78404462cb02fad",
+    "url": "/static/media/apple-black-and-white-camera-196659.c53ad51c.jpg"
+  },
+  {
+    "revision": "03f09eb6d6a0f04af2a9",
+    "url": "/static/js/main.03f09eb6.chunk.js"
+  },
+  {
+    "revision": "1dd8e19128667828d156cf5e9066dd53",
+    "url": "/static/media/facebook-instagram.1dd8e191.jpg"
+  },
+  {
+    "revision": "a0a410f72d8135feef13526cf862ccfa",
+    "url": "/static/media/adventure.a0a410f7.jpg"
+  },
+  {
+    "revision": "8c88c4cb2725eacda709ba2e639ad1c3",
+    "url": "/static/media/blur-close-up.8c88c4cb.jpg"
+  },
+  {
+    "revision": "8b07c96eb49fcf515c1b35e1eab79eea",
+    "url": "/static/media/linkedind account.8b07c96e.jpg"
+  },
+  {
+    "revision": "0f8750f412d5d645df546231e4ba47a6",
+    "url": "/index.html"
+  }
+];
